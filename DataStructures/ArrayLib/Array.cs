ab5d53dae62c12d1f45c49c99d10046c7a533039
Mert Gülle <91943281+Zaywreck@users.noreply.github.com>
2023-03-06 15:44:20 +0300

    :)

--- DataStructures/ArrayLib/Array.cs
+++ DataStructures/ArrayLib/Array.cs
@@ -6,24 +6,31 @@ public class Array
     // Type : Array
     private Object[] _InnerArray;
     private int _Index = 0;
-    public int Count => _Index++;
+    public int Count => _Index;
+    public int Capacity => _InnerArray.Length;
     public Array()
     {
         _InnerArray = new Object[4];
     }
     public void Add(Object item)
     {
-        if(_Index > _InnerArray.Length)
+        if(_Index == _InnerArray.Length)
         {
-            Console.WriteLine("Limit exceeded! From now on you will start to override the current content");
-            _Index = 0;
+            DoubleArray(_InnerArray);
         }
-        else
-        {
-            _InnerArray[_Index] = item;
-            _Index++;
-        }
+       
+        _InnerArray[_Index] = item;
+        _Index++;
+      
     }
+
+    private void DoubleArray(object[] array)
+    {
+        var newArray = new Object[array.Length * 2];
+        System.Array.Copy(array, newArray, array.Length);
+        _InnerArray = newArray;
+    }
+
     public void Delete(int index)
     {
         if(_Index > _InnerArray.Length)
--- DataStructures/ArrayTest/ArrayTests.cs
+++ DataStructures/ArrayTest/ArrayTests.cs
@@ -16,5 +16,24 @@ namespace ArrayTest
             //
             Assert.Equal(2, count);
         }
+        [Fact]
+        public void Array_Add_Test()
+        {
+            // Arrange
+            var array = new Array.Array();
+            array.Add("Mert");
+            array.Add("Sena");
+            array.Add("Sena Nur");
+            array.Add("Sena Gülle");
+            array.Add("Mert Gülle");
+
+            // Act
+            int count = array.Count;
+
+            //
+            Assert.Equal(5, count);
+            Assert.Equal(8, array.Capacity);
+
+        }
     }
 }
